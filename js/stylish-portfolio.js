8901a2a98448f3f2d2876c1a6e931c64c676a2ef
piloteer44 [Emil] <emilandersen@live.com>
2021-03-04 12:01:12 +0100

    fixed navbar overlap when scrolling

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -1,6 +1,14 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -8,7 +16,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 114
         }, 1000, "easeInOutExpo");
         return false;
       }
