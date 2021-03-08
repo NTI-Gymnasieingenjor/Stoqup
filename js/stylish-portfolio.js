9a90b1e5415f88fdd1afd801de7e4fbe3c8bc1e5
piloteer44 [Emil] <emilandersen@live.com>
2021-03-08 15:09:51 +0100

    made navbar responsive for mobile

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -62,3 +62,18 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+$("[data-trigger]").on("click", function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  var offcanvas_id =  $(this).attr('data-trigger');
+  $(offcanvas_id).toggleClass("show");
+  $('body').toggleClass("offcanvas-active");
+  $(".screen-overlay").toggleClass("show");
+}); 
+
+$(".btn-close, .screen-overlay").click(function(e){
+  $(".screen-overlay").removeClass("show");
+  $(".mobile-offcanvas").removeClass("show");
+  $("body").removeClass("offcanvas-active");
+}); 
